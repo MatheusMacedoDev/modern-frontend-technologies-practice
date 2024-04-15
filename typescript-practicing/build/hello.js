5a8fe7b0757d5b48edcfb6871df15a2364fe2ebf
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-14 22:29:49 -0300

    Studying a little bit

--- typescript-practicing/build/hello.js
+++ typescript-practicing/build/hello.js
@@ -1,5 +1,5 @@
 function sayHello(personName) {
     return "Hello, ".concat(personName);
 }
-var personName = "Matheus Macedo";
-console.log(sayHello(personName));
+var name1 = "Matheus Macedo";
+console.log(sayHello(name1));
--- typescript-practicing/hello.ts
+++ typescript-practicing/hello.ts
@@ -2,6 +2,28 @@ function sayHello(personName: string) {
     return `Hello, ${personName}`;
 }
 
-let personName = "Matheus Macedo";
+let name2: string = "Matheus Macedo Santos";
+console.log(sayHello(name2));
 
-console.log(sayHello(personName));
+let array: string[];
+
+array = ['a', 'b'];
+
+console.log(array);
+
+function printNumber(number: string | number) {
+    console.log(number);
+}
+
+printNumber("435435");
+
+type Point = {
+    x: number,
+    y: number
+}
+
+function printPoint(point: Point) {
+    console.log(`(${point.x}, ${point.y})`);
+}
+
+printPoint({ x: 4, y: 3 });
