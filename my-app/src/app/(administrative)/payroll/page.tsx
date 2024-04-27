64782f27b6146df48cac6ec1924765fd3465031d
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-04-26 21:42:54 -0300

    Creating a handle error page

--- my-app/src/app/(administrative)/payroll/page.tsx
+++ my-app/src/app/(administrative)/payroll/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+
+import { useEffect } from 'react';
+
 export default function PayrollPage() {
+    useEffect(() => {
+        const number: number = Math.round(Math.random() * 100 - 1 + 1);
+
+        if (number <= 50) {
+            throw new Error();
+        }
+    }, []);
+
     return <div>PayrollPage</div>;
 }
